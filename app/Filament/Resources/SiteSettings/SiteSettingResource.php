ee8c00e90c02d3eaa40abc58406a0f606aa522b9
Heykaa <AqfHklRzk17@gmail.com>
2026-01-09 19:18:21 +0800

    Update SiteSettingResource.php

--- app/Filament/Resources/SiteSettings/SiteSettingResource.php
+++ app/Filament/Resources/SiteSettings/SiteSettingResource.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace App\Filament\Resources\SiteSettings;
 
 use App\Filament\Resources\SiteSettings\SiteSettingResource\Pages;
 use App\Models\SiteSetting;
 use Filament\Forms;
-use Filament\Schemas\Schema;  // Changed from Filament\Forms\Form
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -13,19 +14,20 @@ class SiteSettingResource extends Resource
 {
     protected static ?string $model = SiteSetting::class;
 
+    // Filament v4: type mesti ikut parent Resource
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-cog-6-tooth';
     protected static ?string $navigationLabel = 'Site Settings';
     protected static string|\UnitEnum|null $navigationGroup = 'Settings';
 
-    // Changed: Form $form -> Schema $schema
-    public static function form(Schema $schema): Schema
+    public static function form(Form $form): Form
     {
-        return $schema
+        return $form
             ->schema([
                 Forms\Components\Section::make('Brand')
                     ->schema([
                         Forms\Components\TextInput::make('brand_name')
                             ->maxLength(255),
+
                         Forms\Components\FileUpload::make('favicon_path')
                             ->disk('public')
                             ->directory('site')
@@ -37,16 +39,21 @@ class SiteSettingResource extends Resource
                     ->schema([
                         Forms\Components\TextInput::make('hero_title')
                             ->maxLength(255),
+
                         Forms\Components\Textarea::make('hero_subtitle')
                             ->rows(3),
+
                         Forms\Components\TextInput::make('hero_cta_text')
                             ->maxLength(255),
+
                         Forms\Components\TextInput::make('hero_cta_url')
                             ->maxLength(255),
+
                         Forms\Components\FileUpload::make('hero_image_path')
                             ->disk('public')
                             ->directory('site/hero')
                             ->image(),
+
                         Forms\Components\FileUpload::make('hero_video_path')
                             ->disk('public')
                             ->directory('site/hero')
@@ -60,6 +67,7 @@ class SiteSettingResource extends Resource
                             ->schema([
                                 Forms\Components\TextInput::make('label')
                                     ->required(),
+
                                 Forms\Components\TextInput::make('url')
                                     ->required(),
                             ])
