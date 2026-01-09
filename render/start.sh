0448825d85115e196def4a696b9bca7b55e5b56e
Heykaa <AqfHklRzk17@gmail.com>
2026-01-09 10:21:55 +0800

    Create start.sh

---
+++ render/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
+php artisan key:generate --force || true
+php artisan package:discover || true
+php artisan migrate --force || true
+php artisan storage:link || true
+
+php-fpm -D
+nginx -g 'daemon off;'
