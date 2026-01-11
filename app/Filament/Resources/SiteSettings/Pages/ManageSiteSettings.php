aaad95c389b9fdcdaec98bbe99c5bf254561c55f
Heykaa <AqfHklRzk17@gmail.com>
2026-01-11 15:54:27 +0800

    Update ManageSiteSettings.php

--- app/Filament/Resources/SiteSettings/Pages/ManageSiteSettings.php
+++ app/Filament/Resources/SiteSettings/Pages/ManageSiteSettings.php
@@ -4,9 +4,9 @@ namespace App\Filament\Resources\SiteSettings\Pages;
 
 use App\Filament\Resources\SiteSettings\SiteSettingResource;
 use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ManageRecords;
+use Filament\Resources\Pages\ListRecords;
 
-class ManageSiteSettings extends ManageRecords
+class ManageSiteSettings extends ListRecords
 {
     protected static string $resource = SiteSettingResource::class;
 
