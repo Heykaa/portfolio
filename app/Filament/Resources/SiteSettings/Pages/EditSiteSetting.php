f3870e5a30f5081d6bab42e8474a8cd419df9939
Heykaa <AqfHklRzk17@gmail.com>
2026-01-12 11:54:42 +0800

    Create EditSiteSetting.php

---
+++ app/Filament/Resources/SiteSettings/Pages/EditSiteSetting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SiteSettings\Pages;
+
+use App\Filament\Resources\SiteSettings\SiteSettingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSiteSetting extends EditRecord
+{
+    protected static string $resource = SiteSettingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make()->hidden(), // singleton: jangan delete
+        ];
+    }
+}
